9bea29b1936b4e2108ce1da00e7a85735aef7b3d
Oscar-21 <austin.conder@outlook.com>
2023-03-28 11:05:44 -0400

    start work

--- step_1.sh
+++ step_1.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
-#########################################################
-#    Create an option group so that                     #
-#    we can restore backup etc                          #
-#########################################################
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <SOURCE_DATABASE> <TARGET_DATABASE>"
+	echo -e "\tNote: Do not change the order of parameters."
+	echo -e "\n\tExample: $0 dev-database new-database"
+	exit 1
+fi
+
+SOURCE_DATABASE="$1"
+TARGET_DATABASE="$2"
+
+START_STRING=""
 
 #########################################################
-#    Get engine name and version                        #
+#    Get Source Database Engine and Engine Version      #
 #########################################################
 
-# Source Database
-aws rds describe-db-instances --no-paginate --db-instance-identifier mlssdevsql --query 'DBInstances[0].[Engine,EngineVersion]'
+# Source Engine
+SOURCE_ENGINE="${START_STRING} $(aws rds describe-db-instances --region "${REGION}" --db-instance-identifier "${SOURCE_DATABASE}" --query 'DBInstances[0].Engine' --output text)" || { echo "Failed to run aws rds describe-db-instances commandline, exiting..."; exit 1; }
+[ "${SOURCE_ENGINE}x" == "x" ] && { echo "There is no engine found for the given source database ${SOURCE_DATABASE} in region ${REGION}, exiting..."; exit 1; }
 
-# Target Database
-aws rds describe-db-instances --no-paginate --db-instance-identifier mlssdevsql --query 'DBInstances[0].[Engine,EngineVersion]'
+# Source Engine Version
+SOURCE_ENGINE_VERSION="${START_STRING} $(aws rds describe-db-instances --region "${REGION}" --db-instance-identifier "${SOURCE_DATABASE}" --query 'DBInstances[0].EngineVersion' --output text)" || { echo "Failed to run aws rds describe--db-instances commandline, exiting..."; exit 1; }
+[ "${SOURCE_ENGINE_VERSION}x" == "x" ] && { echo "There is no engine version found for the given source database ${SOURCE_DATABASE} in region ${REGION}, exiting..."; exit 1; }
+
+#########################################################
+#    Get Target Database Engine and Engine Version      #
+#########################################################
+
+# Target Engine
+TARGET_ENGINE="${START_STRING} $(aws rds describe-db-instances --region "${REGION}" --db-instance-identifier "${TARGET_DATABASE}" --query 'DBInstances[0].Engine' --output text)" || { echo "Failed to run aws rds describe-db-instances commandline, exiting..."; exit 1; }
+[ "${TARGET_ENGINE}x" == "x" ] && { echo "There is no engine found for the given source database ${TARGET_DATABASE} in region ${REGION}, exiting..."; exit 1; }
+
+# Target Engine Version
+TARGET_ENGINE_VERSION="${START_STRING} $(aws rds describe-db-instances --region "${REGION}" --db-instance-identifier "${TARGET_DATABASE}" --query 'DBInstances[0].EngineVersion' --output text)" || { echo "Failed to run aws rds describe--db-instances commandline, exiting..."; exit 1; }
+[ "${TARGET_ENGINE_VERSION}x" == "x" ] && { echo "There is no engine version found for the given source database ${TARGET_DATABASE} in region ${REGION}, exiting..."; exit 1; }
 
 # Create Option Group
-aws rds create-option-group \
-    --option-group-name testoptiongroup \
-    --engine-name oracle-ee \
-    --major-engine-version 12.1 \
-    --option-group-description "Test option group"
-
-
+#aws rds create-option-group \
+#  --option-group-name testoptiongroup \
+#  --engine-name oracle-ee \
+#  --major-engine-version 12.1 \
+#  --option-group-description "Test option group"
